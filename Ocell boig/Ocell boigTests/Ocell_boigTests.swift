c447abdd68f04565992934db28b8e2d7a9ea37be
Marc Llopart Riera <larsnow@gmail.com>
2017-01-22 19:28:13 -0500

    nitial commit

---
+++ Ocell boig/Ocell boigTests/Ocell_boigTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ocell_boigTests.swift
+//  Ocell boigTests
+//
+//  Created by Marc Llopart Riera on 1/22/17.
+//  Copyright © 2017 The Fox Game Studio. All rights reserved.
+//
+
+import XCTest
+@testable import Ocell_boig
+
+class Ocell_boigTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
